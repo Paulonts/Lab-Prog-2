da0d670d08d3b923fa7cb0cb686ed7ae3bec4f57
Paulonts <paulo.henrique.nts@gmail.com>
2025-02-26 19:58:49 -0300

    atualizar

--- aula4/exemplo4.c
+++ aula4/exemplo4.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+
+void executarOpcao(char *opcao){
+    if (strcmp(opcao, "iniciar") == 0){
+        printf("iniciando o sistema");
+    }else if(strcmp(opcao, "parar") == 0){
+        printf("parando o sistema");
+    }else if(strcmp(opcao, "reiniciar") == 0){
+        printf("reiniciando o sistema");
+    }else{
+        printf("opcao invalida");
+    }
+    
+}
+
+int main(){
+    char opcao[20];
+
+    printf("digite a opcao desejada: iniciar, parar, reiniciar");
+    scanf("%s", opcao);
+    executarOpcao(opcao);
+
+    return 0;
+}
